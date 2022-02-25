675c5e0939bf2d09806ab026bdf8a7dfcf10cbd6
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-02-25 13:04:57 +0100

    Query getAllUsers with GraphQLList - complete

--- src/schema/Queries/User.ts
+++ src/schema/Queries/User.ts
@@ -1,14 +1,15 @@
+import { UserType } from './../TypeDefs/User';
 import { Users } from './../../entities/users';
-import { GraphQLString } from "graphql";
+import { GraphQLList, } from "graphql";
 
 export const GET_ALL_USERS = {
 
-  type: GraphQLString,
+  type: new GraphQLList(UserType),
 
   async resolve() {
     const result = await Users.find();
-    console.log(result);
-    return 'users list';
+    //console.log(result);
+    return result;
   }
 
 };
