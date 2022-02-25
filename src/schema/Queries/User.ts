eb4f92d9e840658bfbffb74298712eee27bd7402
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-02-25 13:00:48 +0100

    getAllUsers from db

--- src/schema/Queries/User.ts
+++ src/schema/Queries/User.ts
@@ -1,10 +1,13 @@
+import { Users } from './../../entities/users';
 import { GraphQLString } from "graphql";
 
 export const GET_ALL_USERS = {
-  
+
   type: GraphQLString,
 
-  resolve() {
+  async resolve() {
+    const result = await Users.find();
+    console.log(result);
     return 'users list';
   }
 
